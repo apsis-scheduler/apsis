5d6657eea0eb5909463e96eb4b1d0b69969d506c
Augustus Ostow <augustus.ostow@tudor.com>
2025-07-17 16:30:29 -0400

    Set APSIS_HOST=localhost in integration tests

--- test/int/conftest.py
+++ test/int/conftest.py
@@ -1,2 +1,11 @@
 # Required for pytest to set CWD properly for imports from test scripts.
 
+import os
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def set_apsis_host(monkeypatch):
+    """Set APSIS_HOST=localhost for all integration tests."""
+    monkeypatch.setenv("APSIS_HOST", "localhost")
+
